a5c07f132401cc392ce3fc48251eac6c8f30fedb
Craig Donavin <crdonavin@gmail.com>
2019-09-13 01:32:53 +0000

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,25 @@
+class ArtistsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+    def artist_params
+      params.require(:artist).permit(:name, :bio)
+    end
+
+end

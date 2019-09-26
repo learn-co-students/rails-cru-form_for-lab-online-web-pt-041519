02e8b15cbeb63233f51a5d826b372dbc5c5e5da1
ariannakosten <arianna.kosten@gmail.com>
2019-09-25 17:17:43 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,36 @@
 Rails.application.routes.draw do
+  # get 'songs/index'
+
+  # get 'songs/new'
+
+  # get 'songs/create'
+
+  # get 'songs/show'
+
+  # get 'songs/edit'
+
+  # get 'songs/update'
+
+  # get 'genres/new'
+
+  # get 'genres/create'
+
+  # get 'genres/show'
+
+  # get 'genres/edit'
+
+  # get 'genres/update'
+
+  # get 'artists/new'
+
+  # get 'artists/create'
+
+  # get 'artists/show'
+
+  # get 'artists/edit'
+
+  # get 'artists/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs, :genres, :artists
 end
